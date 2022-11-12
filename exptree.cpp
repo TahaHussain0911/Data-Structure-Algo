e0a8f46c288bfc6af172a0bdd4f48825e4889a80
Taha <tahahussain0805@gmail.com>
2022-11-12 16:43:32 +0500

    Procedural to OOP

--- exptree.cpp
+++ exptree.cpp
@@ -1,93 +1,103 @@
 #include <iostream>
 #include <string>
 #include <cstring>
+#include <stack>
 using namespace std;
-
-struct Node
+class ExpTree
 {
-    char ch;
-    Node *RightCH, *LeftCH;
+private:
+    struct Node
+    {
+        char ch;
+        Node *RightCH, *LeftCH;
+    };
+    string postfixexp;
+    Node *root = NULL;
+
+    int isOperator(char c)
+    {
+        if (c == '*' || c == '+' || c == '-' || c == '/' || c == '^' || c == '%')
+        {
+            return 0;
+        }
+        return -1;
+    }
+
+public:
+    ExpTree(string exp)
+    {
+        this->postfixexp = exp;
+    }
+    Node *getroot()
+    {
+        return root;
+    }
+    void PostfixTOTree()
+    {
+        stack <Node *> s;
+        const int postfixlen = postfixexp.length();
+        for (int i = 0; i < postfixlen; i++)
+        {
+            Node *ptr = new Node();
+            ptr->RightCH = NULL, ptr->LeftCH = NULL;
+            ptr->ch = postfixexp[i];
+            if (isOperator(postfixexp[i]) == -1)
+            {
+                s.push(ptr);
+            }
+            else
+            {
+                ptr->RightCH = s.top();
+                s.pop();
+                ptr->LeftCH = s.top();
+                s.pop();
+                s.push(ptr);
+            }
+        }
+        root = s.top();
+    }
+    void inOrder(Node *temproot)
+    {
+        if (temproot->LeftCH != NULL)
+        {
+            inOrder(temproot->LeftCH);
+        }
+        cout << temproot->ch << " ";
+        if (temproot->RightCH != NULL)
+        {
+            inOrder(temproot->RightCH);
+        }
+    }
+    void PreOrder(Node *temproot)
+    {
+        cout << temproot->ch << " ";
+        if (temproot->LeftCH != NULL)
+        {
+            inOrder(temproot->LeftCH);
+        }
+        if (temproot->RightCH != NULL)
+        {
+            inOrder(temproot->RightCH);
+        }
+    }
+    void PostOrder(Node *temproot)
+    {
+        if (temproot->LeftCH != NULL)
+        {
+            inOrder(temproot->LeftCH);
+        }
+        if (temproot->RightCH != NULL)
+        {
+            inOrder(temproot->RightCH);
+        }
+        cout << temproot->ch << " ";
+    }
 };
-
-Node *root = NULL;
-int isOperator(char c)
-{
-    if (c=='*' || c=='+' || c=='-' || c=='/' || c=='^' || c=='%')
-    {
-        return 0;
-    }
-    return -1;
-}
-void PostfixTOTree(string postfixexpression)
-{
-    Node *stack[5];
-    int top = -1;
-    const int postfixlen = postfixexpression.length();
-    char *postfix = new char[postfixlen];
-    strcpy(postfix, postfixexpression.c_str());
-    for (int i = 0; i < sizeof(postfix) - 1; i++)
-    {
-        Node *ptr = new Node();
-        ptr->RightCH = NULL, ptr->LeftCH = NULL;
-        ptr->ch = postfixexpression[i];
-        if (isOperator(postfixexpression[i]) == -1)
-        {
-            top++;
-            stack[top] = ptr;
-            // cout<<stack[top];
-        }
-        else
-        {
-            ptr->RightCH = stack[top];
-            top--;
-            ptr->LeftCH = stack[top];
-            // top--;
-            stack[top] = ptr;
-            // cout<<stack[top];
-        }
-    }
-    root = stack[top];
-}
-void inOrder(Node *temproot)
-{
-    if (temproot->LeftCH != NULL)
-    {
-        inOrder(temproot->LeftCH);
-    }
-    cout << temproot->ch << " ";
-    if (temproot->RightCH != NULL)
-    {
-        inOrder(temproot->RightCH);
-    }
-}
-void PreOrder(Node *temproot)
-{
-    cout << temproot->ch << " ";
-    if (temproot->LeftCH != NULL)
-    {
-        inOrder(temproot->LeftCH);
-    }
-    if (temproot->RightCH != NULL)
-    {
-        inOrder(temproot->RightCH);
-    }
-}
-void PostOrder(Node *temproot)
-{
-    if (temproot->LeftCH != NULL)
-    {
-        inOrder(temproot->LeftCH);
-    }
-    if (temproot->RightCH != NULL)
-    {
-        inOrder(temproot->RightCH);
-    }
-    cout << temproot->ch << " ";
-}
 
 int main()
 {
     string expression = "ab+cd-*";
-    PostfixTOTree(expression);
-    inOrder(root);
+    ExpTree obj(expression);
+    obj.PostfixTOTree();
+    obj.inOrder(obj.getroot());
 }
